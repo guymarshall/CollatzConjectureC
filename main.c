fe3e88b9c8e302e593ac59299af061b1658fd5bc
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-21 10:57:17 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int collatz(int number);
+
+int main()
+{
+	printf("Enter a whole number: ");
+	int number;
+	scanf("%i", &number);
+	
+	printf("ALL (0) or ONE (1): ");
+	int choice;
+	scanf("%i", &choice);
+
+	if (choice == 0)
+	{
+		for (int i = 0; i < number; i++)
+		{
+			printf("%i: %i\n", i, collatz(i));
+		}
+	}
+	else if (choice == 1)
+	{
+		printf("%i: %i\n", number, collatz(number));
+	}
+}
+
+int collatz(int number)
+{
+	int counter = 0;
+	while (number > 1)
+	{
+		counter++;
+		if (number % 2 == 0)
+		{
+			number = number / 2;
+		}
+		else
+		{
+			number = (3 * number) + 1;
+		}
+	}
+
+	return counter;
+}
